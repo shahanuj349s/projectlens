3f7493137f52d72a9ef046438117dafc7578d083
shahanuj349s <shahanuj349s@gmail.com>
2024-07-28 18:14:58 +0530

    integrated maps

--- src/components/Maps/SearchLocation.js
+++ src/components/Maps/SearchLocation.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import "./SearchLocation.css";
 import logo from "../../Assets/logo.png";
 import { Link } from "react-router-dom";
+import { IoIosCall } from "react-icons/io";
+import { MdLocationOn } from "react-icons/md";
 
 const locations = {
   Ahmedabad: [
@@ -21,36 +23,25 @@ const locations = {
     {
       shop_name: "R Kumar",
       shop_addr:
-        "Shangrila Arcade, 14, 100 Feet Anand Nagar Rd, Near Shyamal Crossroads, Satellite",
-      shop_number: "90990 87153",
-      link: "R+Kumar+Opticians-+Optical+Store+in+Satellite",
+        "3, Sigma-2, Sunrise Park Road, Lane Opposite to Himalaya Mall, Vastrapur Rd, below Bon Homie Restaurant",
+      shop_number: "079-40301345",
+      link: "R+Kumar+Opticians-+Optical+Store+in+Vastrapur",
     },
     {
       shop_name: "R Kumar",
       shop_addr:
-        "Shangrila Arcade, 14, 100 Feet Anand Nagar Rd, Near Shyamal Crossroads, Satellite",
-      shop_number: "90990 87153",
-      link: "R+Kumar+Opticians-+Optical+Store+in+Satellite",
+        "Swastik Super Market, 1, Ashram Rd, opp. Sharma Hyundai, Shreyas Colony, Navrangpura",
+      shop_number: "079-26588497",
+      link: "R+Kumar+Opticians-+Optical+Store+in+Ashram",
     },
     {
-      shop_name: "Veena optical Co.",
-      shop_addr: "GR 7-8 , Kothari pol , Gandhi road , Khadia",
-      shop_number: "079-22144797",
-      link: "Veena+Optical+Co",
+      shop_name: "R Kumar",
+      shop_addr:
+        "Pariseema Complex, 15, Chimanlal Girdharlal Rd, next to Jade Blue, Ellisbridge",
+      shop_number: "079-26401755",
+      link: "R+Kumar+Opticians-+Optical+Store+in+CG+Road",
     },
-    {
-      shop_name: "Veena optical Co.",
-      shop_addr: "GR 7-8 , Kothari pol , Gandhi road , Khadia",
-      shop_number: "079-22144797",
-      logo: "../../Assets/logo.png",
-      link: "Veena+Optical+Co",
-    },
-    {
-      shop_name: "Veena optical Co.",
-      shop_addr: "GR 7-8 , Kothari pol , Gandhi road , Khadia",
-      shop_number: "079-22144797",
-      link: "Veena+Optical+Co",
-    },
+    
     // Add more locations for Ahmedabad
   ],
   Gandhinagar: [
@@ -110,19 +101,25 @@ function SearchLocation() {
                   <div>
                     <img src={logo} alt="logo" />
                   </div>
-                  <div className="card-container w-100">
-                    <h5 className="card-title">{shop.shop_name}</h5>
-                    <p className="card-text">{shop.shop_addr}</p>
-                    <div className="card-div">
-                      <p className="card-numb">{shop.shop_number}</p>
+                  <div className="card-container search-card-container w-100">
+                    <h5 className="card-title search-card-title">{shop.shop_name}</h5>
+                    <p className="card-text search-card-text">{shop.shop_addr}</p>
+                    <div className="search-card-div">
+                      <div className="d-flex align-items-center gap-1">
+                        <IoIosCall />
+                        <div className="search-card-numb">{shop.shop_number}</div>
+                      </div>
 
-                      <Link
-                        to={`https://www.google.com/maps/search/?api=1&query=${shop.link}`}
-                        target="_blank"
-                        className="text-primary"
-                      >
-                        Get directions
-                      </Link>
+                      <div className="d-flex align-items-center gap-1">
+                        <MdLocationOn />
+                        <Link
+                          to={`https://www.google.com/maps/search/?api=1&query=${shop.link}`}
+                          target="_blank"
+                          className="text-primary"
+                        >
+                          Get directions
+                        </Link>
+                      </div>
                     </div>
                   </div>
                 </div>
